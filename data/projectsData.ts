b790b3ea6adfb7b4fe41a2a663c6793946ecd27c
soaryong <soaryong.c@gmail.com>
2024-11-28 12:16:48 +0900

    add projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,38 +7,53 @@ interface Project {
 
 const projectsData: Project[] = [
   {
+    title: 'RippleMarket',
+    description: `3rd place winner in the "XRPL SEA HACKATHON 2024" hackathon 🏆 "EVM Sidechain" track`,
+    href: 'https://dorahacks.io/buidl/18380',
+  },
+  {
+    title: 'XChainShop',
+    description: `2nd place winner with Agoric Orchestration 🛒 Honorable Mentions — Seda in the "2024 Chain Abstraction" hackathon 🏆`,
+    href: 'https://github.com/juniahn-dev/xchainshop',
+  },
+  {
+    title: 'DataDiscovery',
+    description: `1st place winner 🥇 PYUSD — Buidl with PYUSD in the "2024 Build the New Internet" hackathon`,
+    href: 'https://devfolio.co/projects/discovey-5c68',
+  },
+  {
     title: 'OrderWrap',
-    description: `🛒 Honorable Mentions — Safe&Magic, Coinbase, WalletConnect in "2024 Build the New Internet" hackathon 🏆`,
+    description: `🛒 Honorable Mentions — Safe&Magic, Coinbase, and WalletConnect in the "2024 Build On" hackathon 🏆`,
     href: 'https://devfolio.co/projects/orderwrap-2eb8',
   },
   {
     title: 'Ludium - Edu Bounty Management System',
-    description: `2nd place winner in "EDU Chain Hackathon: Semester 1" hackathon “EARN" track 🏆`,
+    description: `2nd place winner in the "EDU Chain Hackathon: Semester 1" hackathon “EARN" track 🏆`,
     href: 'https://dorahacks.io/buidl/14752',
   },
   {
     title: 'Ludium - Cosmos-SDK Dapp Development Guide',
-    description: `Documentation for Cosmos Dapp development as part of the Road-to-Dubai project 📚.`,
+    description: `Documentation for Cosmos Dapp development as part of the Road-to-Dubai project 📚`,
     href: 'https://github.com/Ludium-Official/road-to-dubai',
   },
   {
     title: 'nftinfo.🟢nline',
-    description: `3rd place winner in "Celestia Infinite Space Bazaar" hackathon “Build Whatever" track 🚀`,
+    description: `3rd place winner in the "Celestia Infinite Space Bazaar" hackathon “Build Whatever" track 🚀`,
     href: 'https://dorahacks.io/ko/buidl/12604',
   },
   {
     title: 'Dynamic Boy',
-    description: `Dynamic bounty track winner in "ETHGlobal: Frameworks" hackathon 🏆`,
+    description: `Dynamic bounty track winner in the "ETHGlobal: Frameworks" hackathon 🏆`,
     href: 'https://ethglobal.com/showcase/dynamic-boy-xpwo0',
   },
   {
     title: 'Discovey',
-    description: `Safe — Best Use of Safe{Core} for AA bounty track winner in "ETHGlobal 2023 Online" hackathon 🏆`,
+    description: `Safe — Best Use of Safe{Core} for the AA bounty track winner in the "ETHGlobal 2023 Online" hackathon 🏆`,
     href: 'https://ethglobal.com/showcase/discovey-tuywc',
   },
   {
     title: 'Suiquid',
-    description: `Wildcard winners in "2023 Sui Builder House SEOUL" hackathon 🏆`,
+    description: `Wildcard winners in the "2023 Sui Builder House SEOUL" hackathon 🏆`,
     href: 'https://github.com/soaryong/suiquid-html5-game',
   },
 ]
